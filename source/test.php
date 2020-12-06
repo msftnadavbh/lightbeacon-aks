34eafc1f3acc791e8bdb96995ff6f9e29454af43
Nadav Ben Haim [Microsoft] <nadavbh@microsoft.com>
2020-12-06 17:01:05 +0200

    Update test.php

--- source/test.php
+++ source/test.php
@@ -4,16 +4,10 @@
  </head>
  <body>
 <body bgcolor="#f1f1f1" text="Blue">
-<img src="https://nadavstrg.blob.core.windows.net/public/docker-gif-.gif" alt="Docker">
-<h1>If you're seeing this, your website works!</h1>
+<h1>I'm just a test webpage</h1>
 <?php
 echo "<br>";
-echo 'IP Address is ';
-$external_ip = exec('curl http://ipecho.net/plain; echo');
-echo $_SERVER['REMOTE_ADDR'];
-echo "<br>";
-echo 'Your browser is ';
-echo $_SERVER['HTTP_USER_AGENT'];
+echo 'Congratz, your website is working! ';
 echo "<br>";
 
 ?> 
